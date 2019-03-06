2377bef03b3a3414dddc9ae4c35983353edc2ed2
olivier <rapidhands@gmail.com>
2019-03-06 07:17:01 +0100

    Tests-PSCodeHealth
    Tests-PSCodeHealth

--- File/Function Get-FileReport.ps1
+++ File/Function Get-FileReport.ps1
@@ -1,8 +1,9 @@
-﻿Function Get-FileReport {
+﻿Function Get-FileReport
+{
     <#
 .SYNOPSIS
 This function creates detailed file report and exports it in CSV format.
- 
+
 .DESCRIPTION
 Use this function to export properties for files of certain type (doc,txt,jpg,etc.), and files older than certain days.
 The report includes the following file properties:
@@ -13,23 +14,23 @@ The report includes the following file properties:
  -File Owner
  -Last Change Date
  -Computer Name
-    
+
 .PARAMETER Path
 Specifies the target folder path. Scans the files recursively.
-   
+
 .PARAMETER Days
 Defines the file age.
-    
+
 .PARAMETER Ext
 Specifies the file extension.
-   
+
 .PARAMETER Export
 Exports the results in CSV file. Specifies the folder path for the export file.
-   
+
 .EXAMPLE
 Get-FileReport -path c:\backups
 Returns the properties of all files under c:\backups.
- 
+
 File_Name        : filer_backup.txt
 Full_File_Path   : C:\backups\filer_backup.txt
 File_Extension   : .txt
@@ -37,16 +38,16 @@ File_Size_(KB)   : 0.06
 File_Owner       : DOMAIN\user
 Last_Change_Date : 03/12/2013
 Computer_Name    : COMPUTER03
- 
- 
+
+
 .EXAMPLE
 Get-FileReport -path c:\backups -days 1500
- 
+
 Description
 -----------
 Returns all files under c:\backups older than 1500 days.
 The maximum value is 15000.
- 
+
 File_Name        : Rmtshare.exe
 Full_File_Path   : C:\backups\Rmtshare.exe
 File_Extension   : .exe
@@ -54,14 +55,14 @@ File_Size_(KB)   : 12.77
 File_Owner       : DOMAIN\user
 Last_Change_Date : 02/19/1999
 Computer_Name    : COMPUTER03
- 
+
 .EXAMPLE
 Get-FileReport -path c:\backups -days 150 -ext txt
- 
+
 Description
 -----------
 Returns all .txt files under c:\backups older than 150 days.
- 
+
 File_Name        : temp.txt
 Full_File_Path   : C:\backups\temp_files\temp.txt
 File_Extension   : .txt
@@ -69,14 +70,14 @@ File_Size_(KB)   : 0.01
 File_Owner       : DOMAIN\user
 Last_Change_Date : 02/19/2013
 Computer_Name    : COMPUTER03
- 
+
 .EXAMPLE
 Get-FileReport -path c:\docs -days 150 -ext txt -export c:\temp
- 
+
 Description
 -----------
 Exports to CSV all .txt files under c:\backups older than 150 days.
- 
+
 File_Name        : temp.txt
 Full_File_Path   : C:\backups\temp_files\temp.txt
 File_Extension   : .txt
@@ -84,15 +85,15 @@ File_Size_(KB)   : 0.01
 File_Owner       : DOMAIN\user
 Last_Change_Date : 02/19/2013
 Computer_Name    : COMPUTER03
- 
+
 c:\temp\CZBRN3059-File-Report-7_20_2014.csv created successfully.
- 
-.NOTES 
+
+.NOTES
 File Name: Get-FileReport.ps1
 Author: Nikolay Petkov
 Blog: http://77.104.138.174/~powershe/power-shell.com
 Last Edit: 12/17/2014
- 
+
 .LINK
 http://77.104.138.174/~powershe/power-shell.com
  #>
@@ -108,16 +109,30 @@ http://77.104.138.174/~powershe/power-shell.com
         [Parameter(Mandatory = $False)]
         [string]$Export
     )
-    try {
-        $hostname = gc env:computername
-        If ($days) {$olderthan = (Get-Date).AddDays(-$days)}
-        else {$olderthan = Get-Date}
+    try
+    {
+        $hostname = Get-Content env:computername
+        If ($days)
+        {
+            $olderthan = (Get-Date).AddDays(-$days)
+        }
+        else
+        {
+            $olderthan = Get-Date
+        }
         $fileColl = @()
         # Get the files
-        If ($Ext) {$files = Get-ChildItem -path $path -recurse -filter *.$ext -ErrorAction stop | Where-Object {$_.CreationTime -lt $olderthan}}
-        else {$files = Get-ChildItem -Path $path -Recurse -ErrorAction stop | Where-Object {!$_.PSIsContainer -and $_.CreationTime -lt $olderthan}}
+        If ($Ext)
+        {
+            $files = Get-ChildItem -path $path -recurse -filter *.$ext -ErrorAction stop | Where-Object {$_.CreationTime -lt $olderthan}
+        }
+        else
+        {
+            $files = Get-ChildItem -Path $path -Recurse -ErrorAction stop | Where-Object {!$_.PSIsContainer -and $_.CreationTime -lt $olderthan}
+        }
         # Retreive properties for each file
-        foreach ($file in $files) {
+        foreach ($file in $files)
+        {
             $fileObject = New-Object PSObject #An object,created and destroyed for each file
             $networkpath = "\\" + $HostName + $path.substring($path.LastIndexOf('\')) + '\' + (($file.FullName).trim($path))
             #The following add data to the fileObjects
@@ -128,20 +143,37 @@ http://77.104.138.174/~powershe/power-shell.com
             Add-Member -inputObject $fileObject -memberType NoteProperty -name "File_Owner" -value (get-acl $file.Fullname).Owner
             Add-Member -inputObject $fileObject -memberType NoteProperty -name "Last_Change_Date" -value $file.LastWriteTime.ToString("MM/dd/yyy")
             Add-Member -inputObject $fileObject -memberType NoteProperty -name "Computer_Name" -value $hostname
-            $fileObject |sort Last_Change_Date #Output to the screen for a visual feedback
+            $fileObject |Sort-Object Last_Change_Date #Output to the screen for a visual feedback
             $fileColl += $fileObject #Copy the contents of the object into the Array
             $fileObject = $null #Delete the fileObject
         } #end foreach
-        If ($fileColl) {Write-Host "File properties successfully retrieved." -ForegroundColor Green}
-        If ($export) {
+        If ($fileColl)
+        {
+            Write-Output "File properties successfully retrieved."
+        }
+        If ($export)
+        {
             $csvname = (($olderthan.tostring()).split('')[0]).replace('/', '_')
             $fileColl | Export-Csv -path "$export\$HostName-Files-Older-Than-$csvname.csv" -NoTypeInformation
         }
     }
-    catch {
-        If (!(Test-Path $Path)) {Write-Host "Cannot find ""$path"" because it does not exist." -ForegroundColor Red}
-        If (!($files)) {Write-Host "No $Ext files found older than $olderthan." -ForegroundColor Green}
-        If (($Export) -and (!(Test-Path $Export))) {Write-Host "Cannot export data to ""$Export"" because it does not exist." -ForegroundColor Red}
+    catch
+    {
+        If (!(Test-Path $Path))
+        {
+            Write-Output "Cannot find ""$path"" because it does not exist."
+        }
+        If (!($files))
+        {
+            Write-Output "No $Ext files found older than $olderthan."
+        }
+        If (($Export) -and (!(Test-Path $Export)))
+        {
+            Write-Output "Cannot export data to ""$Export"" because it does not exist."
+        }
     }
-    If (Test-Path $export\$HostName-Files-Older-Than-$csvname.csv) {Write-Host "$export\$HostName-Files-Older-Than-$csvname.csv created successfully." -ForegroundColor Green}
+    If (Test-Path $export\$HostName-Files-Older-Than-$csvname.csv)
+    {
+        Write-Output "$export\$HostName-Files-Older-Than-$csvname.csv created successfully."
+    }
 } #end function Get-FileReport
--- File/Function Read-OpenFileDialog.ps1
+++ File/Function Read-OpenFileDialog.ps1
@@ -1,4 +1,5 @@
-﻿function Read-OpenFileDialog() { 
+﻿function Read-OpenFileDialog()
+{
     <#
         .DESCRIPTION
         Permet d'ouvrir une zone de dialogue afin de choisir un fichier et retourne le fichier sélectionné par l'utilisateur
@@ -10,20 +11,30 @@
                    IntelGFXa
         .NOTES
         Fonction à appeler dans un script pour sélectionner un dossier
-        #>   
+        #>
 
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
-        [string]$InitialDirectory, 
+        [string]$InitialDirectory,
         [switch]$AllowMultiSelect
-    ) 
+    )
 
     Add-Type -AssemblyName System.Windows.Forms
     $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $openFileDialog.initialDirectory = $InitialDirectory
     $openFileDialog.filter = "All files (*.*)| *.*"
-    if ($AllowMultiSelect) { $openFileDialog.MultiSelect = $true }
+    if ($AllowMultiSelect)
+    {
+        $openFileDialog.MultiSelect = $true
+    }
     $openFileDialog.ShowDialog() > $null
-    if ($allowMultiSelect) { return $openFileDialog.Filenames } else { return $openFileDialog.Filename }
+    if ($allowMultiSelect)
+    {
+        return $openFileDialog.Filenames
+    }
+    else
+    {
+        return $openFileDialog.Filename
+    }
 }
